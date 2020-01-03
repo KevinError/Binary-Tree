d6b8b4c3714c6157f46aff5a74fd2e6869a058d8
Kevin <khk4084417@gmail.com>
2020-01-02 20:25:32 -0800

    Created PreorderTraversal and TreeNode classes

---
+++ PreorderTraversal.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+public class PreorderTraversal {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+    }
+
+
+    public static void main(String[] args)
+    {
+
+    }
+}
