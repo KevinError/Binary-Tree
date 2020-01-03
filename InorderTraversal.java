1b95fb8c7ee0ea2a674d0fb012d0ee4424589191
Kevin <khk4084417@gmail.com>
2020-01-03 07:33:14 -0800

    completes PreorderTraversal, starting InorderTraversal class

---
+++ InorderTraversal.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.Stack;
+import java.util.ArrayList;
+
+public class InorderTraversal{
+
+    public static void main(String[] args)
+    {
+
+    }
+}
--- PreorderTraversal.java
+++ PreorderTraversal.java
@@ -1,14 +1,31 @@
 import java.util.List;
+import java.util.Stack;
 import java.util.ArrayList;
 
 public class PreorderTraversal {
 
+    /**
+     * Return the list of node using Pre-order Traversal.
+     * @param root
+     * @return list of nodes
+     */
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> nodeList = new ArrayList<Integer>();
-        while (root.left == null || root.right == null)
+        Stack<TreeNode> treeList = new Stack<TreeNode>();
+        while (root != null || !treeList.empty())
         {
-            
+            if (root == null)
+            {
+                root = treeList.pop().right;
+            }
+            if (root.right != null)
+            {
+                treeList.push(root);
+            }
+            nodeList.add(root.val);
+            root = root.left;
         }
+        
         return nodeList;
 
     }
