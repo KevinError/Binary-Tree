50ef266c754fd1fafb4e9f074a73b51b15a7ab41
Kevin <khk4084417@gmail.com>
2020-01-04 07:06:19 -0800

    solves the Inorder-traversal class

--- InorderTraversal.java
+++ InorderTraversal.java
@@ -4,6 +4,23 @@ import java.util.ArrayList;
 
 public class InorderTraversal{
 
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List < Integer > res = new ArrayList < > ();
+        Stack < TreeNode > stack = new Stack < > ();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+        
+    }
+
     public static void main(String[] args)
     {
 
